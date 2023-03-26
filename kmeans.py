286db31c6d484491070ff73e2b8206f32062a32e
sanjupau <114467652+sanjupau@users.noreply.github.com>
2023-03-27 01:42:17 +0530

    Update kmeans.py

--- kmeans.py
+++ kmeans.py
@@ -28,3 +28,5 @@ sns.relplot(
     x="Longitude", y="Latitude", hue="Cluster", data=X, height=5,
 );
 
+
+![kmeans](https://user-images.githubusercontent.com/114467652/227801909-ce9efbd7-6f51-419d-ad08-779b5bd90df6.jpg)
