e87481635f0fb50458134f5dfb9cf9351c6c78b0
sanjupau <114467652+sanjupau@users.noreply.github.com>
2023-05-07 14:00:59 +0530

    Update decisiontree.py

--- decisiontree.py
+++ decisiontree.py
@@ -1,32 +1,52 @@
-import pandas as pd
-from sklearn.metrics import accuracy_score, confusion_matrix
-from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
+import pandas as pd 
+import numpy as np 
+import matplotlib.pyplot as plt 
+from sklearn import metrics 
+import seaborn as sns 
+from sklearn.datasets import load_iris 
+from sklearn.model_selection import train_test_split 
+from sklearn import tree 
+from sklearn.metrics import accuracy_score
+iris = load_iris() 
+data = pd.DataFrame(data = iris.data, columns = iris.feature_names) 
+data['Species'] = iris.target 
+ 
+target = np.unique(iris.target) 
+target_n = np.unique(iris.target_names) 
+target_dict = dict(zip(target, target_n)) 
+data['Species'] = data['Species'].replace(target_dict) 
+ 
+x = data.drop(columns = "Species") 
+y = data["Species"] 
+names_features = x.columns 
+target_labels = y.unique() 
+ 
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.3, random_state = 93) 
+ 
+from sklearn.tree import DecisionTreeClassifier 
+ 
+dtc = DecisionTreeClassifier(max_depth = 3, random_state = 93) 
+ 
+dtc.fit(x_train, y_train) 
+ 
+plt.figure(figsize = (30, 10), facecolor = 'b') 
+Tree = tree.plot_tree(dtc, feature_names = names_features, class_names = target_labels, rounded = 
+True, filled = True, fontsize = 14) 
+plt.show() 
+y_pred = dtc.predict(x_test) 
 
-
-data= pd.read_csv("humiduty.csv")
-
-data.columns
-data
-
-
-data.shape
-del data['Water']
-data.shape
-
-data.shape
-clean_data= data.copy()
-clean_data['enjoyspot'] = (clean_data['Humidity']>23)*1
-clean_data['enjoyspot']
-clean_data
-y=clean_data['enjoyspot'].copy()
-y.columns
-y
-
-other_features =['Sky','AirTemp','Wind','Forecast']
-x=clean_data[other_features].copy()
-x.columns
-y.columns
-X_train, Y_train, X_test, Y_test = train_test_split(x,y,test_size=0.33, random_state= 324)
-y_predicted = humanity.predict(X_test)
-accuracy_score(Y_test,y_predicted)*100
+confusion_matrix = metrics.confusion_matrix(y_test, y_pred) 
+matrix = pd.DataFrame(confusion_matrix) 
+axis = plt.axes() 
+sns.set(font_scale = 1.3) 
+plt.figure(figsize = (10,7)) 
+ 
+sns.heatmap(matrix, annot = True, fmt = "g", ax = axis, cmap = "magma") 
+axis.set_title('Confusion Matrix') 
+axis.set_xlabel("Predicted Values", fontsize = 10) 
+axis.set_xticklabels([''] + target_labels) 
+axis.set_ylabel( "True Labels", fontsize = 10) 
+axis.set_yticklabels(list(target_labels), rotation = 0) 
+plt.show() 
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
